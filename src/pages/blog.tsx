438edac83e71b63a165724a284e664f3256bd08e
daveg7lee <daveg7lee@gmail.com>
2022-11-03 18:57:30 +0900

    Page Query

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,30 @@
+import { graphql, PageProps } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
-export default function Blog() {
+export default function Blog({ data }: PageProps<Queries.BlogTitlesQuery>) {
   return (
     <Layout title="Blog">
-      <p>The most recent news from my shop.</p>
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>
+            <p>{file.name}</p>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
