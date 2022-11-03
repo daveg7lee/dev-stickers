f273ad0043e37846b7425a8983d83dffc3118645
daveg7lee <daveg7lee@gmail.com>
2022-11-03 21:18:43 +0900

    Blog Post

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,32 @@
+import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../../components/Layout";
+import Seo from "../../components/Seo";
 
-export default function BlogPost() {
-  return (
-    <Layout title="Blog Post">
-      <div></div>
-    </Layout>
-  );
+interface IBlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: any;
 }
+
+export default function BlogPost({ children }: IBlogPostProps) {
+  return <Layout title="Blog Post">{children}</Layout>;
+}
+
+export const query = graphql`
+  query PostDetail($frontmatter_slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter_slug } }) {
+      body
+      frontmatter {
+        author
+        category
+        date
+        slug
+        title
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: IBlogPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title!} />
+);
