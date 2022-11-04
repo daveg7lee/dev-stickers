508e2a70b9013b40376e39374b96c80290ed8d37
daveg7lee <daveg7lee@gmail.com>
2022-11-05 07:59:45 +0900

    Blog Post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
@@ -6,6 +7,10 @@ export default function IndexPage() {
   return (
     <Layout title="Home">
       <p>Welcome to DevStickers!</p>
+      <StaticImage
+        src="https://images.unsplash.com/photo-1540651810471-5699df74834f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80"
+        alt="Stickers"
+      />
     </Layout>
   );
 }
